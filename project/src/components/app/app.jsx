b5479f99da4c48d19bc468489e2b96af241fed3e
Pihta-Hvojnaja <pavel.chirihin.87@mail.ru>
2021-06-03 16:25:18 +0300

    Добавит компоненты: app, mainPage, smallFilmCard

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import MainPage from '../main-page/main-page';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+
+function App(props) {
+  const { movieData } = props;
+
+  return (
+    <MainPage movieData = {movieData} />
+  );
 }
+
+
+App.propTypes = {
+  movieData: PropTypes.object.isRequired,
+};
+
 
 export default App;
